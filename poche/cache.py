a8e4f10da69d467933bed788be3fe354e33b3919
etienne-napoleone <etienne.napoleone@gmail.com>
2020-01-01 01:04:24 +0900

    feat(cache): add ttl

--- poche/cache.py
+++ poche/cache.py
@@ -1,17 +1,44 @@
+from datetime import datetime
+from datetime import timedelta
 from typing import Any
 from typing import Dict
+from typing import Optional
+from typing import Tuple
 
 
 class Cache:
-    _store: Dict[str, Any] = {}
+    _store: Dict[str, Tuple[Optional[datetime], Any]] = {}
 
     @staticmethod
-    def set(key: str, value: Any, override: bool = True):
+    def set(
+        key: str, value: Any, ttl: Optional[int] = None, override: bool = True
+    ):
         if override:
-            Cache._store[key] = value
+            Cache._store[key] = (
+                Cache.get_expiration(ttl) if ttl else None,
+                value,
+            )
         else:
-            Cache._store.setdefault(key, value)
+            Cache._store.setdefault(
+                key, (Cache.get_expiration(ttl) if ttl else None, value)
+            )
 
     @staticmethod
     def get(key: str) -> Any:
-        return Cache._store.get(key, None)
+        value_tuple = Cache._store.get(key)
+        print(value_tuple)
+        if not value_tuple:
+            return None
+        elif isinstance(value_tuple[0], datetime):
+            if value_tuple[0] > datetime.now():
+                return value_tuple[1]
+            else:
+                del Cache._store[key]
+                return None
+        else:
+            print("no ttl")
+            return value_tuple[1]
+
+    @staticmethod
+    def get_expiration(ttl: int) -> datetime:
+        return datetime.now() + timedelta(seconds=ttl)
--- tests/test_cache.py
+++ tests/test_cache.py
@@ -2,32 +2,33 @@ from poche.cache import Cache
 
 KEY = "test_key"
 VALUE = 1
+VALUE_TUPLE = (None, VALUE)
 
 
 def test_set():
     Cache.set(KEY, VALUE)
-    assert Cache._store[KEY] == VALUE
+    assert Cache._store[KEY] == VALUE_TUPLE
     Cache._store = {}
 
 
 def test_set_override():
     Cache.set(KEY, VALUE)
-    assert Cache._store[KEY] == VALUE
+    assert Cache._store[KEY] == VALUE_TUPLE
     Cache.set(KEY, "override")
-    assert Cache._store[KEY] == "override"
+    assert Cache._store[KEY] == (None, "override")
     Cache._store = {}
 
 
 def test_set_override_disabled():
     Cache.set(KEY, VALUE)
-    assert Cache._store[KEY] == VALUE
+    assert Cache._store[KEY] == VALUE_TUPLE
     Cache.set(KEY, "override", override=False)
-    assert Cache._store[KEY] == VALUE
+    assert Cache._store[KEY] == VALUE_TUPLE
     Cache._store = {}
 
 
 def test_get():
-    Cache._store[KEY] = VALUE
+    Cache._store[KEY] = VALUE_TUPLE
     assert Cache.get(KEY) == VALUE
     Cache._store = {}
 
