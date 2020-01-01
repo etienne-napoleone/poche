14a47ce436a6165f98f6fb275e2f21efe9a642eb
etienne-napoleone <etienne.napoleone@gmail.com>
2020-01-01 17:20:05 +0900

    feat(cacheitem): add magic comparison methods

--- poche/cacheitem.py
+++ poche/cacheitem.py
@@ -8,3 +8,21 @@ from typing import Optional
 class Cacheitem:
     expiration: Optional[datetime]
     value: Any
+
+    def __eq__(self, other: object) -> bool:
+        return self.value == getattr(other, "value", other)
+
+    def __ne__(self, other: object) -> bool:
+        return not self.__eq__(other)
+
+    def __lt__(self, other: object) -> bool:
+        return self.value < getattr(other, "value", other)
+
+    def __gt__(self, other: object) -> bool:
+        return self.value > getattr(other, "value", other)
+
+    def __le__(self, other: object) -> bool:
+        return self.value <= getattr(other, "value", other)
+
+    def __ge__(self, other: object) -> bool:
+        return self.value >= getattr(other, "value", other)
--- tests/test_cacheitem.py
+++ tests/test_cacheitem.py
@@ -1,9 +1,62 @@
 from datetime import datetime
 
+import pytest
+
 from poche.cacheitem import Cacheitem
+
+ITEM = Cacheitem(datetime.now(), 1)
 
 
 def test_instantiate():
-    item = Cacheitem(datetime.now(), "value")
-    assert item.expiration
-    assert item.value
+    assert ITEM.expiration
+    assert ITEM.value
+
+
+def test_magic_eq():
+    assert ITEM == ITEM
+    assert ITEM == Cacheitem(None, 1)
+    assert ITEM == 1
+    assert not ITEM == "1"
+
+
+def test_magic_ne():
+    assert not ITEM != ITEM
+    assert not ITEM != Cacheitem(None, 1)
+    assert not ITEM != 1
+    assert ITEM != "1"
+
+
+def test_magic_lt():
+    assert ITEM < Cacheitem(None, 2)
+    assert ITEM < 2
+    assert not ITEM < 0
+    with pytest.raises(TypeError):
+        assert ITEM < "2"
+
+
+def test_magic_gt():
+    assert ITEM > Cacheitem(None, 0)
+    assert ITEM > 0
+    assert not ITEM > 2
+    with pytest.raises(TypeError):
+        assert ITEM > "0"
+
+
+def test_magic_le():
+    assert ITEM <= Cacheitem(None, 2)
+    assert ITEM <= 2
+    assert ITEM <= Cacheitem(None, 1)
+    assert ITEM <= 1
+    assert not ITEM <= 0
+    with pytest.raises(TypeError):
+        assert ITEM <= "2"
+
+
+def test_magic_ge():
+    assert ITEM >= Cacheitem(None, 0)
+    assert ITEM >= 0
+    assert ITEM >= Cacheitem(None, 1)
+    assert ITEM >= 1
+    assert not ITEM >= 2
+    with pytest.raises(TypeError):
+        assert ITEM >= "0"
